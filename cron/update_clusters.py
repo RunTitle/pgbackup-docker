c2a9e601e97e9fca44dc94711997c49a1883d3d1
Stephen Durham <sdurham@runtitle.com>
2016-07-20 16:33:54 -0500

    Adding another queue.

--- cron/update_clusters.py
+++ cron/update_clusters.py
@@ -5,18 +5,19 @@ from queued.manager import QueuedManager
 
 
 def update_queue():
-    update_queue = 'generate_clusters'
+    update_queues = ['generate_clusters', 'calculate_daily_metrics']
     queue_manager = QueuedManager(
         config={'env': os.getenv('ENVIRONMENT', 'development')},
         aws_owner=os.getenv('AWS_OWNER'),
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
         aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
         application='titletime',
-        subscriptions=['generate_clusters'],
-        publications=['generate_clusters']
+        subscriptions=['generate_clusters', 'calculate_daily_metrics'],
+        publications=['generate_clusters', 'calculate_daily_metrics']
     )
     message = {'task': 'update_queue'}
-    queue_manager.publish_message(name=update_queue, raw_message=message)
+    for update_queue in update_queues:
+        queue_manager.publish_message(name=update_queue, raw_message=message)
 
 if __name__ == '__main__':
     update_queue()
